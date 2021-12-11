d1f884bc54e61716629fe6cb8af0fd7e291f2dd4
dpanhey <d.panhey@gmx.de>
2021-12-11 22:41:55 +0100

    added function computerPlay for the random generated computer answer, added two consts, one for playerSelection (rock) and one for computerSelection

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 "use strict";
 
-console.log("Hello world!");
+// This Game is about beating the opponent with the right choice:
+// Rock, Paper or Scissor.
+// Rock beats scissor, scissor beats paper and paper beats rock.
+
+// First function: the computer should randomly return "Rock", "Paper" or "Scissor".
+
+function computerPlay() {
+    const answers = [
+        "rock",
+        "paper",
+        "scissor"
+    ];
+    const randomElement = Math.floor(Math.random() * answers.length);
+    return answers[randomElement];
+}
+
+// declare a const playerSelection, so that the player is prompted for rock, paper or scissor
+// declare a const computerSelection wich takes the output from computerPlay
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+
+console.log(playerSelection);
+console.log(computerPlay());
+
+// Second function: A function that plays a single round of Rock Paper Scissor
+
+// Third function: A function that returns a string that declares the winner of this round
+// the function declareWinner should return a string like "You Lose! Paper beats Rock!"
+
