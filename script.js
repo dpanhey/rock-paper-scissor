2ca7692dd26423fcd2273c321b1563ba1da99fc0
dpanhey <d.panhey@gmx.de>
2022-01-07 16:58:45 +0100

    index: added 3 buttons, script: deleted play game logic and first comments

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
 "use strict";
 
-// This Game is about beating the opponent with the right choice:
-// Rock, Paper or Scissor.
-// Rock beats scissor, scissor beats paper and paper beats rock.
-
-// First function: the computer should randomly return "Rock", "Paper" or "Scissor".
-
-// declare a const playerSelection, so that the player is prompted for rock, paper or scissor
-// declare a const computerSelection wich takes the output from computerPlay
-
-// console.log(playerSelection);
-// console.log(computerPlay());
-
-
-// Second function: A function that plays a single round of Rock Paper Scissor
-// Third function: A function that returns a string that declares the winner of this round
-// the function declareWinner should return a string like "You Lose! Paper beats Rock!"
-
-// Write a function called game() which have the previos functions and consts inside of it
-// make this function play the game 5 times
-
 function game() {
     function computerPlay() {
         const answers = [
@@ -65,9 +45,3 @@ function game() {
     }
     console.log(playRound(playerSelection, computerSelection))
 }
-
-game();
-game();
-game();
-game();
-game();
