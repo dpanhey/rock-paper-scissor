c2243f34605a0d507b78f3a8750f69dabccf958e
dpanhey <d.panhey@gmx.de>
2022-01-09 22:15:51 +0100

    script: changed the whole logic, made the UI dynamic, with tracked score and an 'reset' event that triggers at a score of 5

--- script.js
+++ script.js
@@ -1,27 +1,71 @@
 "use strict";
 
-function computerPlay() {
+const buttons = document.querySelectorAll("#buttons button");
+const infoText = document.querySelector("div#info-screen p")
+const playerScoreInfo = document.querySelector("div#score p:first-child")
+const computerScoreInfo = document.querySelector("div#score p:last-child")
+
+let playerScore = 0;
+let computerScore = 0;
+let gameOver = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.id, computerChoice())
+        checkWinCondition();
+        resetGame();
+    });
+});
+
+function computerChoice() {
     const answers = [
-        "rock",
-        "paper",
-        "scissor"
+        "aard",
+        "axii",
+        "igni"
     ];
     const randomElement = Math.floor(Math.random() * answers.length);
     return answers[randomElement];
-}
+};
 
-const computerSelection = computerPlay();
-const playerSelection = "";
-
-function checkWinCondition(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-        return `Its a draw! Both have ${playerSelection}!`
+        infoText.textContent = `Its a draw! Both have ${playerSelection}!`;
     } else if (
-        playerSelection == "rock" && computerSelection == "scissor" ||
-        playerSelection == "scissor" && computerSelection == "paper" ||
-        playerSelection == "paper" && computerSelection == "rock") {
-        return `You win! ${playerSelection} beats ${computerSelection}!`
+        playerSelection == "aard" && computerSelection == "axii" ||
+        playerSelection == "axii" && computerSelection == "igni" ||
+        playerSelection == "igni" && computerSelection == "aard") {
+            playerScore += 1;
+            playerScoreInfo.textContent = playerScore;
+            infoText.textContent = `You win! ${playerSelection} beats ${computerSelection}!`;
     } else {
-        return `You loose! ${computerSelection} beats ${playerSelection}!`
+        computerScore += 1;
+        computerScoreInfo.textContent = computerScore;
+        infoText.textContent = `You loose! ${computerSelection} beats ${playerSelection}!`;
     }
-}
+};
+
+function checkWinCondition () {
+    if (playerScore == 5) {
+        infoText.textContent = "You are worthy to enter! Please come in and take an elixier!"
+        gameOver = 1;
+    } else if (computerScore == 5) {
+        infoText.textContent = "You are not worthy! Go, get some practice!"
+        gameOver = 1;
+    }
+};
+
+function resetGame () {
+    if (gameOver) {
+        const button = document.querySelectorAll("button");
+        const buttonParent = document.querySelector("div#buttons");
+        button.forEach((button) => {
+            button.parentNode.removeChild(button);
+        })
+        let createButton = document.createElement("button");
+        createButton.setAttribute("class", "buttons");
+        createButton.setAttribute("id", "newGame")
+        createButton.textContent = "Let's play again!";
+        createButton = buttonParent.appendChild(createButton);
+        console.log(buttons);
+    }
+};
