ffb8a10cd9709f20c780d9250d4c55cdb6712e66
dpanhey <d.panhey@gmx.de>
2022-01-09 20:43:53 +0100

    deleted old functions and improved the 'checkWinCondition' function

--- script.js
+++ script.js
@@ -1,47 +1,27 @@
 "use strict";
 
-function game() {
-    function computerPlay() {
-        const answers = [
-            "rock",
-            "paper",
-            "scissor"
-        ];
-        const randomElement = Math.floor(Math.random() * answers.length);
-        return answers[randomElement];
+function computerPlay() {
+    const answers = [
+        "rock",
+        "paper",
+        "scissor"
+    ];
+    const randomElement = Math.floor(Math.random() * answers.length);
+    return answers[randomElement];
+}
+
+const computerSelection = computerPlay();
+const playerSelection = "";
+
+function checkWinCondition(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return `Its a draw! Both have ${playerSelection}!`
+    } else if (
+        playerSelection == "rock" && computerSelection == "scissor" ||
+        playerSelection == "scissor" && computerSelection == "paper" ||
+        playerSelection == "paper" && computerSelection == "rock") {
+        return `You win! ${playerSelection} beats ${computerSelection}!`
+    } else {
+        return `You loose! ${computerSelection} beats ${playerSelection}!`
     }
-
-    function playerInput () {
-        const input = prompt("Take a choice!").toLowerCase();
-        return input;
-    }
-
-    const computerSelection = computerPlay();
-    const playerSelection = playerInput();
-
-    console.log(playerSelection);
-    console.log(computerSelection);
-
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection == "rock" && computerSelection == "rock") {
-            return "Its a draw! Both have Rock!"
-        } else if (playerSelection == "rock" && computerSelection == "paper") {
-            return "You loose! Paper beats Rock!"
-        } else if (playerSelection == "rock" && computerSelection == "scissor") {
-            return "You win! Rock beats Scissor!"
-        } else if (playerSelection == "paper" && computerSelection == "paper") {
-            return "Its a draw! Both have Paper!"
-        } else if (playerSelection == "paper" && computerSelection == "scissor") {
-            return "You loose! Scissor beats Paper!"
-        } else if (playerSelection == "paper" && computerSelection == "rock") {
-            return "You win! Paper beats Rock!"
-        } else if (playerSelection == "scissor" && computerSelection == "scissor") {
-            return "Its a draw! Both have Scissor!"
-        } else if (playerSelection == "scissor" && computerSelection == "rock") {
-            return "You loose! Rock beats Scissor!"
-        } else if (playerSelection == "scissor" && computerSelection == "paper") {
-            return "You win! Scissor beats Paper!"
-        }
-    }
-    console.log(playRound(playerSelection, computerSelection))
 }
